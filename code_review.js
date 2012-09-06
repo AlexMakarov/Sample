c40592677d33a44bdd32c0fc6f333f198bb876d3
unknown <alexander.makarov@o14.rp.ru>
2012-09-06 17:42:08 +0400

    One more commit from developer

--- code_review.js
+++ code_review.js
@@ -1,17 +1,17 @@
-function old1(){
-//
-// Old code
-//
-}
-
-function old2(){
-//
-// Old code
-//
-}
-
 function bad(){
 //
 // Bad code
+//
+}
+
+function good(){
+//
+//
+//
+}
+
+function wait_review(){
+//
+//
 //
 }
